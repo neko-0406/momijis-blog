4823f522d996c6693d4af1feac8d7b92969213c0
momiji <sateraito3696@gmail.com>
2026-04-30 19:53:42 +0900

    数式とmarmaidの図表示に対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
-
 import react from '@astrojs/react';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), keystatic(), react()],
+    integrations: [mdx(), keystatic(), react(), mermaid()],
     output: 'static',
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    },
 });
